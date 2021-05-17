64d948642fe67b6fc0dfcb59f9ffd9a6439699a8
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-17 13:26:31 +0530

    String example with different way.
    OUTPUT:
    Hello I'm Azharuddin Sk.
    The length is: 13
    UPPER CASE: AZHARUDDIN SK
    LOWER CASE: azharuddin sk

---
+++ stringExample.dart
@@ -0,0 +1,10 @@
+//String example
+
+
+void main(){
+  String name='Azharuddin Sk';  //declear the sring
+  print("Hello I'm $name."); //print the string value 
+  print("The length is: ${name.length}");  //calcuate the length of string
+  print("UPPER CASE: ${name.toUpperCase()}");  //upper case of string
+  print("LOWER CASE: ${name.toLowerCase()}"); ///lower case of string
+}
