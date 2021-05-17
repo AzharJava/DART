63b42cc7b128d540b3bca6794f05c8192f965bdb
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-17 13:34:42 +0530

    final variable example with valid output
    OUTPUT:
    NAME: Azharuddin Sk
    AGE: 23

---
+++ finalwamplewithgood.dart
@@ -0,0 +1,11 @@
+//final variable valid example.
+
+
+void main(){
+  final name='Azharuddin Sk'; //one person one name this is not changeable
+  var age=23;  //age may be change in the future
+  print("NAME: $name");
+  print("AGE: $age");
+  
+
+}
