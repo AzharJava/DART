4096403e20fafde298161e5c15d944b10e81f2a6
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-17 13:32:46 +0530

    final variable example
    OUTPUT:
    Error compiling to JavaScript:
    Warning: Interpreting this as package URI, 'package:dartpad_sample/main.dart'.
    lib/main.dart:10:3:
    Error: Can't assign to the final variable 'name'.
      name='Umme Salma Khatun';
      ^^^^
    Error: Compilation failed.

---
+++ finalExample.dart
@@ -0,0 +1,12 @@
+//final variable example
+
+
+void main(){
+  final name='Azharuddin Sk'; //one person one name this is not changeable
+  var age=23;  //age may be change in the future
+  print("NAME: $name");
+  print("AGE: $age");
+  
+  name='Umme Salma Khatun';  // it's error
+  print("UPDATE NAME: $name");
+}
