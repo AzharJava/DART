b06a58b313b58c9152e66bf5cba2088f6bb1cb18
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-17 13:29:31 +0530

    Var variable example
    OUTPUT:
    NAME: Azharuddin Sk
    AGE: 23
    HEIGHT: 1.34
    UPDATE AGE: 24

---
+++ varExample.dart
@@ -0,0 +1,12 @@
+//var example
+
+void main(){
+  var name='Azharuddin Sk';
+  var age=23;
+  var height=1.34;
+  print("NAME: $name");
+  print("AGE: $age");
+  print("HEIGHT: $height");
+  age=24; //we can change easily of doing the work
+  print("UPDATE AGE: $age");
+}
