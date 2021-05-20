fba0102bbbb0bd7715f904eb67752bd8d4510065
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-20 12:46:40 +0530

    Map clear example
    OUTPUT:
    Before add: {username: azhar525, password: Skazhar525@}
    After clear details: {}

---
+++ mapClear.dart
@@ -0,0 +1,10 @@
+
+//map property clear()
+void main(){
+  var details= {'username':'azhar525', 'password':'Skazhar525@'};
+  print("Before add: ${details}");
+  
+  details.clear();
+  
+  print('After clear details: ${details}');
+}
