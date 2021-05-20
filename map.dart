b2f2f876334532914c243eeda46cd8edec2acef8
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-20 12:25:33 +0530

    Map constructor
    OUTPUT
    {Username: Azhar525, Password: Azharuddin123@, email: skazhar525@gmail.com}

---
+++ map.dart
@@ -0,0 +1,9 @@
+//Map constructor
+
+void main(){
+var details=new Map();
+  details['Username']='Azhar525';
+  details['Password']='Azharuddin123@';
+  details['email']='skazhar525@gmail.com';
+  print(details);
+}
