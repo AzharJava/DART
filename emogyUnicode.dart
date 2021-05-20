c15c29ebb0264e79ec48d12721fc195bd6237c69
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-20 13:29:33 +0530

    Emogy create through unicode
    OUTPUT:
     😅 
     😀 
     😍 
     😃 
     😁 
     😂 
     😈 
     😉 
     😆 
     😇 
     😂 
     😃

---
+++ emogyUnicode.dart
@@ -0,0 +1,28 @@
+
+main() { 
+   Runes input = new Runes(' \u{1f605} '); 
+  Runes input2 = new Runes(' \u{1f600} '); 
+  Runes input3 = new Runes(' \u{1f60D} '); 
+  Runes input4 = new Runes(' \u{1f603} '); 
+  Runes input5 = new Runes(' \u{1f601} '); 
+  Runes input6 = new Runes(' \u{1f602} '); 
+  Runes input7 = new Runes(' \u{1f608} '); 
+  Runes input8 = new Runes(' \u{1f609} '); 
+  Runes input9 = new Runes(' \u{1f606} '); 
+  Runes input10 = new Runes(' \u{1f607} '); 
+  Runes input11 = new Runes(' \u{1f602} '); 
+  Runes input12 = new Runes(' \u{1f603} '); 
+   print(new String.fromCharCodes(input)); 
+  print(new String.fromCharCodes(input2));
+  print(new String.fromCharCodes(input3));
+  print(new String.fromCharCodes(input4));
+  print(new String.fromCharCodes(input5));
+  print(new String.fromCharCodes(input6));
+  print(new String.fromCharCodes(input7));
+  print(new String.fromCharCodes(input8));
+  print(new String.fromCharCodes(input9));
+  print(new String.fromCharCodes(input10));
+  print(new String.fromCharCodes(input11));
+  print(new String.fromCharCodes(input12));
+  
+} 
