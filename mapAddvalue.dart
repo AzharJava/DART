493777489c0c2ebdff7ad7227468146e98aa06e1
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-20 12:33:00 +0530

    adding value in map
    OUTPUT:
    Before add any value: {username: azhar525, password: azhar525@}
    After the details add: {username: azhar525, password: azhar525@, Uid: azhar525}

---
+++ mapAddvalue.dart
@@ -0,0 +1,14 @@
+
+
+
+//addiing value to map literals
+
+void main(){
+  var details={'username':'azhar525', 'password':'azhar525@'};
+  
+  print("Before add any value: $details");
+  
+  
+  details['Uid']='azhar525';
+  print("After the details add: $details");
+}
